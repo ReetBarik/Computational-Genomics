9b575ffaec6a021b46d120034cff3358806eb157
ReetBarik <reetbarik94@gmail.com>
2019-03-25 15:50:42 -0700

    Bash file to generate Output file

---
+++ P2/Generate_Output.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc API_TestDriver.c -o test;./test s1.fas English_alphabet.txt >> Output.txt;./test s2.fas English_alphabet.txt >> Output.txt;./test colorblind_human_gene.fasta DNA_alphabet.txt >> Output.txt;./test colorblind_mouse_gene.fasta DNA_alphabet.txt >> Output.txt;./test Human-BRCA2-cds.fasta DNA_alphabet.txt >> Output.txt;./test Slyco.fas DNA_alphabet.txt >> Output.txt;./test chr12.fas DNA_alphabet.txt >> Output.txt
