3ea0e196b6d920f214f9c01ed54c16f438214019
ReetBarik <reetbarik94@gmail.com>
2019-04-06 00:19:47 -0700

    Initial commit for P3: working code to read refseq, reads and alphabet

---
+++ P3/Header.h
@@ -0,0 +1,17 @@
+#ifndef header
+#define header
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int alphabetLength = 0;
+int sequenceLength = 0;
+int readsLength = 0;
+char *alphabet;							
+char *sequence;			
+char *readBuffer;
+char **readsList;				
+char sequenceName[16];	
+
+#endif
